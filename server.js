e373e1995fd656e64387f390e2c9c73d37ee419b
sajitharjun82 <75490557+sajitharjun82@users.noreply.github.com>
2021-01-12 19:45:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - Node Js application');
+  res.send('Hello World - Node Js application - updated');
 });
 
 app.listen(PORT, HOST);
